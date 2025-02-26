3d24b440acd64d68373d19ea0137ec188843afd7
efraim.gelder <119013869+efi98@users.noreply.github.com>
2025-02-27 00:44:47 +0200

    feat: Remove Firebase config from prod environment
    Moves Firebase configuration from the production environment
    file to improve security and uses the general environment file
    instead. Also, adds node dependency.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,4 @@
 export const environment = {
   production: true,
-  name: "prod",
-  firebase: {
-    apiKey: "AIzaSyC4oCKTGLbNGj6MvkKJHJSHb5v1hRlboZc",
-    authDomain: "eden-2b9b1.firebaseapp.com",
-    projectId: "eden-2b9b1",
-    storageBucket: "eden-2b9b1.firebasestorage.app",
-    messagingSenderId: "844958245569",
-    appId: "1:844958245569:web:dc7d4ac05eb14e0ef16a40",
-  },
+  name: "prod"
 };
